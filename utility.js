70687d37c9975ae5fc89050c5210fb707dbc55fd
Eecodev <mnasser.bbk@gmail.com>
2023-10-17 15:02:01 +0200

    initial scaffolding

---
+++ utility.js
@@ -0,0 +1,23 @@
+
+
+
+//Genera un numero random
+function getRndInteger(min, max){
+    return Math.floor(Math.random()* (max - min + 1)) + min;
+}
+
+
+/**
+ * Determina se il numero è pari
+ * @param {Number} num 
+ * @returns {Boolean}
+ */
+function isEven(num){
+    // if(num % 2 === 0){
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    // let result = (num % 2 === 0) ? true : false;
+    result = (num % 2 === 0) ? true : false;
+}
